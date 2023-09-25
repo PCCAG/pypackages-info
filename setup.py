04f7da44cde49d8164cafe979c1089425251011b
PCCAG <120740893+PCCAG@users.noreply.github.com>
2023-09-25 20:36:53 +0800

    1

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(),
     author="PCCAG",
     description="A simple tool. Can output the name, version, last modified time, and location of the installed module in the current environment in a tabular form at the terminal.",
+    url="https://github.com/PCCAG/pypackages-info",
     entry_points={
         "console_scripts": [
             "pypackage-info = pypackage_info.info:main",
